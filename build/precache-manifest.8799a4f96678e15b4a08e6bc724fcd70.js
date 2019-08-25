f0748fd2a8c6d3d95b99e8e3de0e25ef5cc5699b
Nick Korintus <nicholas.korintus@gmail.com>
2019-08-25 11:13:16 -0400

    initial commit

---
+++ build/precache-manifest.8799a4f96678e15b4a08e6bc724fcd70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3ff0222d1670cb9faced596f1087e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ece5aee9ec3ac954609",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "30f2cee13d68b3ef05c7",
+    "url": "/static/js/2.88747c88.chunk.js"
+  },
+  {
+    "revision": "8ece5aee9ec3ac954609",
+    "url": "/static/js/main.c60ca3e6.chunk.js"
+  },
+  {
+    "revision": "d0d095e5592b30651d3a",
+    "url": "/static/js/runtime~main.d2da7071.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
